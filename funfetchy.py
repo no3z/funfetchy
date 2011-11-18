19e2b362911651b5600475923f7289b373e45ccf
no3productionz@gmail.com <no3produtionz@gmail.com>
2011-11-18 22:32:30 +0100

    Some advances. Fetching jpgs from reddit/funny

--- funfetchy.py
+++ funfetchy.py
@@ -20,7 +20,6 @@ class RedditSubmissions(db.Model):
   title = db.StringProperty()
   url = db.StringProperty()
   author = db.StringProperty()
-  json = db.StringProperty()
   rand = db.FloatProperty()
 
 
@@ -39,43 +38,44 @@ class FfSlideshow(FfBaseHandler):
   def get(self):
     submissions = db.Query(RedditSubmissions)
     submissions = RedditSubmissions.all()
-    print (submissions.count())
-
+        
     self.render_to_response('templatehtml/index.html', {
         'subs': submissions,
-      })
+     })
 
 
 class FfUpdate(FfBaseHandler):
 
   def get(self):
-    r = reddit.Reddit(user_agent="redfunfetcher")
-    l = list(r.get_subreddit("funny").get_top(limit=300))
+    r = reddit.Reddit(user_agent="redfunfetcher2")
+    l = list(r.get_subreddit("funny").get_top(limit=20))
 
-
-
+ 
     for s in l:
-        print s,int(str(s.url).find(".jpg"))
         if int(str(s.url).find(".jpg")) > 0:
-            img_data = db.Blob(urlfetch.Fetch(str(s.url)).content)
+            print s,s.title,s.url
 
-        if img_data == None:
-            continue
-        try:
-            img = images.Image(img_data)
-            jpg_data = img.execute_transforms(images.JPG)
-            RedditSubmissions(
-            data = jpg_data,title = s.title,
-            url = s.url,  author = s.author,
-            json = db.StringProperty(),   rand = random.random()).put()
-        except:
-            self.error(400)
-            self.response.out.write('Sorry, the image provided was too large for us to process.')
+            image = urlfetch.Fetch(str(s.url)).content
+
+            jpg = db.Blob(images.execute_transforms(image.JPEG))
+
+            try:
+                RedditSubmissions(
+                data = jpg,
+                title = s.title,
+                url = s.url,
+                author = s.author.user_name,
+                rand = random.random()).put()
+
+            except Exception,e:
+                print e
+                self.response.out.write('Sorry, the image provided was too large for us to process.')
+                continue
 
     sub = db.Query(RedditSubmissions)
     sub = RedditSubmissions.all()
     
-    self.render_to_response('templatehtml/upload.html', {'subs': sub })
+    #self.render_to_response('templatehtml/upload.html', {'subs': sub })
 
 
 def main():
---
+++ settings.py
@@ -0,0 +1,8 @@
+# djangostatic/djangostatic/settings.py
+
+import os
+ROOT_PATH = os.path.dirname(__file__)
+
+TEMPLATE_DIRS = (
+    ROOT_PATH + "/templatehtml/",
+)
